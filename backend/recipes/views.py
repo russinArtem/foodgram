e359af8caf4295d311b520a1f8fccb5f853d1344
Artem Russin <russinartem@yandex.ru>
2026-06-28 06:47:55 +0300

    v.3.6

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -7,6 +7,6 @@ from recipes.models import Recipe
 class RecipeShortLinkRedirectView(RedirectView):
     permanent = False
 
-    def get_redirect_url(self, request, *args, **kwargs):
+    def get_redirect_url(self, *args, **kwargs):
         recipe = get_object_or_404(Recipe, id=kwargs['recipe_id'])
-        return request.build_absolute_uri(f'/recipes/{recipe.id}/')
+        return f'/recipes/{recipe.id}/'
